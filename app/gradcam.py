b645bcf1aecb23a7d73e1a3446c07acb0be5401a
anshi_aggrwal <anshiagarwal0207@gmail.com>
2026-03-08 01:14:08 +0530

    updated gradcam

--- app/gradcam.py
+++ app/gradcam.py
@@ -1,41 +1,63 @@
 import matplotlib
 matplotlib.use("Agg")
+
 import cv2
 import numpy as np
 import os
 import uuid
 from PIL import Image
-from pytorch_grad_cam import GradCAMPlusPlus
+
+from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+
 from app.model_loader import model, DEVICE, transform
 
-TARGET_LAYER = model.features[-1] # The last feature of the model is typically the most relevant for Grad-CAM.
+
+TARGET_LAYER = model.features[-1]
+
+# Lazy initialization
 cam = None
+
+
 def generate_gradcam(image_path, class_idx):
     global cam
+
+    # Load image
     image = Image.open(image_path).convert("RGB")
     image_resized = image.resize((224, 224))
-    image_np = np.array(image_resized) / 255.0 #for visualization, we need the image in [0, 1] range.
+    image_np = np.array(image_resized) / 255.0
+
+    # Prepare tensor
     input_tensor = transform(image).unsqueeze(0).to(DEVICE)
 
+    # Initialize GradCAM once
     if cam is None:
-        cam = GradCAMPlusPlus(
-            model = model,   # Initialize Grad-CAM with the loaded model.
-            target_layers = [TARGET_LAYER],
-            use_cuda = (DEVICE == "cuda")
+        cam = GradCAM(
+            model=model,
+            target_layers=[TARGET_LAYER],
+            use_cuda=(DEVICE == "cuda")
         )
-    grayscale_cam = cam(
-        input_tensor = input_tensor,
-        targets = None # no specific class we want to focus on
-    )[0] # Get the CAM for the first (and only) image in the batch.
 
+    # Focus on predicted class
+    targets = [ClassifierOutputTarget(class_idx)]
+
+    # Generate CAM
+    grayscale_cam = cam(
+        input_tensor=input_tensor,
+        targets=targets
+    )[0]
+
+    # Overlay heatmap
     visualization = show_cam_on_image(
         image_np,
         grayscale_cam,
-        use_rgb = True
+        use_rgb=True
     )
 
-    os.makedirs("static/gradcam", exist_ok = True)
+    # Save image
+    os.makedirs("static/gradcam", exist_ok=True)
+
     cam_name = f"cam_{uuid.uuid4()}.jpg"
     cam_path = f"static/gradcam/{cam_name}"
 
@@ -45,4 +67,3 @@ def generate_gradcam(image_path, class_idx):
     )
 
     return cam_path
-
