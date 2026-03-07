480a140ec75e75c663e966d57d45bb60a8d780c4
anshi_aggrwal <anshiagarwal0207@gmail.com>
2026-03-07 18:52:09 +0530

    updated gradcam

--- app/gradcam.py
+++ app/gradcam.py
@@ -9,18 +9,17 @@ from pytorch_grad_cam.utils.image import show_cam_on_image
 from app.model_loader import model, DEVICE, transform
 
 TARGET_LAYER = model.features[-1] # The last feature of the model is typically the most relevant for Grad-CAM.
+cam = GradCAMPlusPlus(
+    model = model,   # Initialize Grad-CAM with the loaded model.
+    target_layers = [TARGET_LAYER],
+    use_cuda = (DEVICE == "cuda")
+)
 
 def generate_gradcam(image_path, class_idx):
     image = Image.open(image_path).convert("RGB")
     image_resized = image.resize((224, 224))
     image_np = np.array(image_resized) / 255.0 #for visualization, we need the image in [0, 1] range.
     input_tensor = transform(image).unsqueeze(0).to(DEVICE)
-
-    cam = GradCAMPlusPlus(
-        model = model,   # Initialize Grad-CAM with the loaded model.
-        target_layers = [TARGET_LAYER],
-        use_cuda = (DEVICE == "cuda")
-    )
 
     grayscale_cam = cam(
         input_tensor = input_tensor,
